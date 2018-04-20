3473fedd014327ef981b575adcc31281815bd07e
willi314 <willi314@muohio.edu>
2018-04-20 00:18:17 -0400

    refactored a bit of the main application

--- src/com/example/PokerHands/PokerHandsComparer.java
+++ src/com/example/PokerHands/PokerHandsComparer.java
@@ -8,20 +8,19 @@ public class PokerHandsComparer {
 	public static void main(String[] args) {
 		String input = "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH";
 		createPokerHands(input);
-		int pokerHandComparisonResult = hand0.compareTo(hand1);
-		System.out.println(createOutputString(pokerHandComparisonResult));
+		System.out.println(createOutputString(hand0.compareTo(hand1)));
+		
 		input = "Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S";
 		createPokerHands(input);
-		pokerHandComparisonResult = hand0.compareTo(hand1);
-		System.out.println(createOutputString(pokerHandComparisonResult));
+		System.out.println(createOutputString(hand0.compareTo(hand1)));
+		
 		input = "Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH";
 		createPokerHands(input);
-		pokerHandComparisonResult = hand0.compareTo(hand1);
-		System.out.println(createOutputString(pokerHandComparisonResult));
+		System.out.println(createOutputString(hand0.compareTo(hand1)));
+		
 		input = "Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH";
 		createPokerHands(input);
-		pokerHandComparisonResult = hand0.compareTo(hand1);
-		System.out.println(createOutputString(pokerHandComparisonResult));
+		System.out.println(createOutputString(hand0.compareTo(hand1)));
 	}
 	
 	private static String createOutputString(int pokerHandComparisonResult){
